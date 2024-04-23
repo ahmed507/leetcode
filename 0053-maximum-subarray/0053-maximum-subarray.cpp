2011a9a09278cf0f526c431b0ab7c9592c78ddfb
Ahmed Hamada <ahmedhamada507@gmail.com>
2024-04-23 13:40:30 +0200

    Time: 74 ms (74.09%), Space: 72.8 MB (5.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-int maxSubArray(vector<int>& nums) {
-    int max_so_far = nums[0];
-    int current_max = nums[0];
-    for (int i = 1; i < nums.size(); i++) {
+int maxSubArrayRecursive(vector<int>& nums, int start, int end, vector<int>& memo) {
+    if (start > end) return INT_MIN;
+    if (start == end) return nums[start];
+    if (memo[start] != -1) return memo[start];
+    int max_so_far = nums[start];
+    int current_max = nums[start];
+    for (int i = start + 1; i <= end; i++) {
         current_max = max(nums[i], current_max + nums[i]);
         max_so_far = max(max_so_far, current_max);
     }
+    memo[start] = max_so_far;
     return max_so_far;
+}
+
+int maxSubArray(vector<int>& nums) {
+    if (nums.size() == 0) return INT_MIN + 1;
+    vector<int> memo(nums.size(), -1);
+    return maxSubArrayRecursive(nums, 0, nums.size() - 1, memo);
 }
 
 };
